5b4bc753c8893d8d14f492cce3b6bb36fa806919
ahmed barkhed <davidmohmed@gmail.com>
2025-03-21 15:20:50 +0400

    update sitmaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+export default {
+  siteUrl: 'https://ahmedbarkhed.vercel.app', // رابط موقعك
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  generateIndexSitemap: false,
+};
