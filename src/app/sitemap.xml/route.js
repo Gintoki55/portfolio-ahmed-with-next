fd0909b056218e9e0c588ebaa49552b901f416ea
ahmed barkhed <davidmohmed@gmail.com>
2025-03-23 14:04:55 +0400

    sitemapss...

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,8 +1,8 @@
-// /src/app/sitemap.js
+// /src/app/sitemap.xml/route.js
 
 const baseUrl = 'https://ahmedbarkhed.vercel.app';
-const locales = ['en', 'ar', 'es', 'it']; // اللغات المتوفرة
-const staticRoutes = ['', '/services', '/experience', '/projects', '/contact']; // أضف هنا مسارات صفحاتك
+const locales = ['en', 'ar', 'es', 'it']; // اللغات المدعومة
+const staticRoutes = ['', '/services', '/experience', '/projects', '/contact']; // المسارات الثابتة
 
 export async function GET() {
   const urls = locales.flatMap((locale) =>
@@ -18,9 +18,9 @@ export async function GET() {
   );
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls.join('\n')}
-  </urlset>`;
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls.join('\n')}
+    </urlset>`;
 
   return new Response(sitemap, {
     headers: {
